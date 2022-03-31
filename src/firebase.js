7006aa36f6457a4ff402d2b3ba2b86887454888e
ajay jangra <68214687+Ajay-Jangra@users.noreply.github.com>
2022-03-31 22:45:12 +0530

    Delete firebase.js

--- src/firebase.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyDEdgToztBrY9c8mKPwVczLOywOFRmuEHE",
-  authDomain: "netflix-clone-21-75c29.firebaseapp.com",
-  projectId: "netflix-clone-21-75c29",
-  storageBucket: "netflix-clone-21-75c29.appspot.com",
-  messagingSenderId: "562155400674",
-  appId: "1:562155400674:web:5a34e6790d988677421dbb",
-  measurementId: "G-T5JPBHG2BS",
-};
